70d18200447902e32178c6e0c6080937d5bb4f8b
wechat <admin@eeob.com>
2019-01-27 18:13:12 +0800

    2019/1/27

--- src/main/java/javakit/application.java
+++ src/main/java/javakit/application.java
@@ -9,17 +9,15 @@ import java.io.IOException;
 public class application {
     public static void main(String args[]) {
         System.out.println("Hello World!");
-        HttpClientResponse.Request("http://api.eeob.com/cinemas/movie/title?title=我不是药神&page=0&size=5", new HttpClientResponse.ResultCallback<ResponseBody>() {
+        HttpClientResponse.String("http://api.eeob.com/cinemas/movie/title?title=我不是药神&page=0&size=5", new HttpClientResponse.ResultCallback<String>() {
             @Override
-            public void success(ResponseBody res) throws IOException  {
-
-                    System.out.println("success:" +res.string());
+            public void success(String res) throws IOException {
 
             }
 
             @Override
             public void failure(Exception e) {
-                System.out.println("failure:" +e.getMessage());
+
             }
         });
 
--- src/main/java/javakit/network/HttpClientResponse.java
+++ src/main/java/javakit/network/HttpClientResponse.java
@@ -8,14 +8,61 @@ import okhttp3.Response;
 
 import java.io.IOException;
 
+import java.io.InputStream;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class HttpClientResponse{
+    public static void bytes(String url,ResultCallback<byte[]> resultCallback) {
 
 
-    public static void Request(String url,ResultCallback<ResponseBody> resultCallback) {
+        FutureTask<byte[]> task = new FutureTask<byte[]>(new Callable<byte[]>() {
+
+            @Override
+            public byte[] call()  {
+                // TODO Auto-generated method stub
+                byte[] bytes = null;
+                Response response = null;
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                try {
+                    response = client.newCall(request).execute();
+                } catch (IOException e) {
+                    resultCallback.failure(e);
+                }
+
+                if (response.isSuccessful()) {
+                    try {
+                        bytes = response.body().bytes();
+                    } catch (IOException e) {
+                        resultCallback.failure(e);
+                    }
+                }else {
+                    resultCallback.failure(new Exception("network request error : " + response.code()));
+                    return null;
+                }
+                return bytes;
+            }});
+        new Thread(task).start();
+
+        try {
+            resultCallback.success(task.get());
+        } catch (InterruptedException e) {
+            resultCallback.failure(e);
+            return;
+        } catch (ExecutionException e) {
+            resultCallback.failure(e);
+            return;
+        } catch (IOException e) {
+            resultCallback.failure(e);
+        }
+    }
+
+    public static void ResponseBody(String url,ResultCallback<ResponseBody> resultCallback) {
 
 
         FutureTask<ResponseBody> task = new FutureTask<ResponseBody>(new Callable<ResponseBody>() {
@@ -43,6 +90,96 @@ public class HttpClientResponse{
                         return null;
                     }
                 return responseBody;
+            }});
+        new Thread(task).start();
+
+        try {
+            resultCallback.success(task.get());
+        } catch (InterruptedException e) {
+            resultCallback.failure(e);
+            return;
+        } catch (ExecutionException e) {
+            resultCallback.failure(e);
+            return;
+        } catch (IOException e) {
+            resultCallback.failure(e);
+        }
+    }
+
+    public static void String(String url,ResultCallback<String> resultCallback) {
+
+        FutureTask<String> task = new FutureTask<String>(new Callable<String>() {
+            @Override
+            public String call()  {
+                // TODO Auto-generated method stub
+                String result = null;
+                Response response = null;
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                try {
+                    response = client.newCall(request).execute();
+                } catch (IOException e) {
+                    resultCallback.failure(e);
+                }
+
+                if (response.isSuccessful()) {
+                    try {
+                        result = response.body().string();
+                    } catch (IOException e) {
+                        resultCallback.failure(e);
+                    }
+                }else {
+                    resultCallback.failure(new Exception("network request error : " + response.code()));
+                    return null;
+                }
+                return result;
+            }});
+        new Thread(task).start();
+
+        try {
+            resultCallback.success(task.get());
+        } catch (InterruptedException e) {
+            resultCallback.failure(e);
+            return;
+        } catch (ExecutionException e) {
+            resultCallback.failure(e);
+            return;
+        } catch (IOException e) {
+            resultCallback.failure(e);
+        }
+    }
+
+    public static void InputStream(String url,ResultCallback<InputStream> resultCallback) {
+
+
+        FutureTask<InputStream> task = new FutureTask<InputStream>(new Callable<InputStream>() {
+
+            @Override
+            public InputStream call()  {
+                // TODO Auto-generated method stub
+                InputStream inputStream = null;
+                Response response = null;
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                try {
+                    response = client.newCall(request).execute();
+                } catch (IOException e) {
+                    resultCallback.failure(e);
+                }
+
+                if (response.isSuccessful()) {
+                    inputStream = response.body().byteStream();
+                }else {
+                    resultCallback.failure(new Exception("network request error : " + response.code()));
+                    return null;
+                }
+                return inputStream;
             }});
         new Thread(task).start();
 
