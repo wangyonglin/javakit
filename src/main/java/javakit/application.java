b1ce38dc7509ebc17258a0631fb5dd631d616499
wechat <admin@eeob.com>
2019-03-03 21:42:02 +0800

    2019/3/3

--- src/main/java/javakit/application.java
+++ src/main/java/javakit/application.java
@@ -1,31 +1,20 @@
 package javakit;
 
 
+import javakit.date.DateUtils;
 import javakit.security.RSAKeys;
 import javakit.security.RSAUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class application {
     public static void main(String args[]) {
         System.out.println("Hello World!");
-        try {
-            String str ="wangyonglin and wangxiaoli";
-            RSAKeys keys= RSAKeys.make();
-            System.out.println("getPrivateKey:"+keys.getPrivateKey());
-            System.out.println("########################");
-            System.out.println("getPublicKey:"+keys.getPublicKey());
-
-           String in = RSAUtils.encryptedDataOnJava(str,keys.getPublicKey());
-
-            System.out.println("encryptedDataOnJava:"+in);
-            String out = RSAUtils.decryptDataOnJava(in,keys.getPrivateKey());
-            System.out.println("decryptDataOnJava:"+out);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
+            Date d= DateUtils.replace("2018-03-01T05:35:13.514Z");
+       int x= DateUtils.differentDays(d,new Date());
+        System.out.println("Hello World! "+x);
     }
 
 }
--- src/main/java/javakit/date/DateUtils.java
+++ src/main/java/javakit/date/DateUtils.java
@@ -10,6 +10,17 @@ public class DateUtils {
     public static Date CurrentDate(){
         return  new Date();
     }
+    public static Date replace(String date){
+        try{
+            date = date.replace("Z", " UTC");//注意是空格+UTC
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS Z");//注意格式化的表达式
+            return format.parse(date );
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
     public static String DateFormat(Date date){
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return formatter.format(date);
@@ -25,5 +36,47 @@ public class DateUtils {
         calendar.setTime(date);
         calendar.add(Calendar.MONTH, amount);
         return calendar.getTime();
+    }
+
+    /**
+     * date2比date1多的天数
+     * @param date1
+     * @param date2
+     * @return
+     */
+    public static int differentDays(Date date1,Date date2)
+    {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        int day1= cal1.get(Calendar.DAY_OF_YEAR);
+        int day2 = cal2.get(Calendar.DAY_OF_YEAR);
+
+        int year1 = cal1.get(Calendar.YEAR);
+        int year2 = cal2.get(Calendar.YEAR);
+        if(year1 != year2)
+        {
+            int timeDistance = 0 ;
+            for(int i = year1 ; i < year2 ; i ++)
+            {
+                if(i%4==0 && i%100!=0 || i%400==0)
+                {
+                    timeDistance += 366;
+                }
+                else
+                {
+                    timeDistance += 365;
+                }
+            }
+
+            return timeDistance + (day2-day1) ;
+        }
+        else
+        {
+
+            return day2-day1;
+        }
     }
 }
